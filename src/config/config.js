2499c870ebbd268357536252e1cfb871c9735456
marcelsafont <marcel.safont1983@gmail.com>
2021-01-22 10:58:45 +0100

    removed default config file from git

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const config = {
     port: process.env.PORT || 3000,
     dburl: 'mongodbtest://database/commerce',
     expiration: 60 * 60, 
-    seed: 'secret'
+    seed: 'secret2'
 }
 
 module.exports = { config }
