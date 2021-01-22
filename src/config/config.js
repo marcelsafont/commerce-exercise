d90723901e9eae0d1ad1888f05235e5eb3f66bb5
marcelsafont <marcel.safont1983@gmail.com>
2021-01-22 11:00:01 +0100

    removed file from repo

--- src/config/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    port: process.env.PORT || 3000,
-    dburl: 'mongodbtest://database/commerce',
-    expiration: 60 * 60, 
-    seed: 'secret2'
-}
-
-module.exports = { config }
